7dcb25ac538d5b7c2e38162e3a6b5c7d2da0907b
sngynhy <sngynhy@gmail.com>
2022-09-18 18:54:59 +0900

    vuetify combobox

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div>
+      <div>
+        <p :style="{ 'font-size': selectedG + 'px' }">큰 타이틀 사이즈</p>
+        <comboBox :selectedSize="selectedG" :sizeList="gList" />
+      </div>
+      <hr />
+      <div>
+        <p :style="{ 'font-size': selectedX + 'px' }">X 타이틀 사이즈</p>
+        <comboBox :selectedSize="selectedX" :sizeList="xyList" />
+      </div>
+      <hr />
+      <div>
+        <p :style="{ 'font-size': selectedY + 'px' }">Y 타이틀 사이즈</p>
+        <comboBox :selectedSize="selectedY" :sizeList="xyList" />
+      </div>
+      <hr />
+      <div>
+        <p :style="{ 'font-size': selectedTick + 'px' }">축의 값 사이즈</p>
+        <comboBox :selectedSize="selectedTick" :sizeList="tickList" />
+      </div>
+      <hr />
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import comboBox from "@/components/combobox.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    comboBox,
+  },
+  data() {
+    return {
+      gList: [18, 20, 22],
+      xyList: [16, 18, 20],
+      tickList: [8, 9, 10],
+      selectedG: 21,
+      selectedX: 17,
+      selectedY: 16,
+      selectedTick: 11,
+    };
+  },
+  methods: {},
+};
+</script>
---
+++ src/components/combobox.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-flex xs1>
+    <v-combobox
+      :items="sizeList"
+      item-text="value"
+      item-value="value"
+      background-color="white"
+      v-model="selectedValue"
+      dense
+      outlined
+      @change="changeValue()"
+    />
+    <div>{{ selectedValue }}</div>
+  </v-flex>
+  <!-- @change="changeValue()" -->
+</template>
+
+<script>
+export default {
+  name: "comboBox",
+  props: ["selectedSize", "sizeList"],
+
+  data() {
+    return {
+      selectedValue: "",
+      savedValue: "",
+      list: {},
+    };
+  },
+  mounted() {
+    this.selectedValue = this.selectedSize;
+  },
+  methods: {
+    changeValue() {
+      this.savedValue = this.selectedValue;
+      this.$emit("");
+    },
+  },
+};
+</script>
+<style scoped>
+.v-input__control .v-text-field__details {
+  display: none;
+}
+</style>
